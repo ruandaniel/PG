9dcbd4c310f1894977f7048e6e884468e00e4bf8
ruandaniel <ruandaniel666@gmail.com>
2017-05-02 17:13:31 -0400

    1

--- 42. 足球比赛.cpp
+++ 42. 足球比赛.cpp
@@ -1,16 +1,17 @@
+//DFS + Memory
 class Solution{
 public:
 	vector<string> allPaths(int m, int n){
 		unordered_map<string, vector<string>> mp;
-		string final = to_string(m) + ' ' + to_string(n);
+		string final = to_string(m) + ':' + to_string(n);
 		mp[final] = { final };
 		return findPath(0, 0, m, n, mp);
 	}
 
 	vector<string> findPath(int a, int b, int m, int n, unordered_map<string, vector<string>> &mp){
-		string s = to_string(a) + ' ' + to_string(b);
+		string s = to_string(a) + ':' + to_string(b);
 		if (mp.count(s)) return mp[s];
-		string ss = s + ",";
+		string ss = s + " | ";
 		vector<string> cur;
 		if (a < m){
 			auto t = findPath(a + 1, b, m, n, mp);
@@ -27,4 +28,29 @@ public:
 		mp[s] = cur;
 		return cur;
 	}
+};
+
+//bottom-up DP
+class Solution{
+public:
+    vector<string> allPaths(int m, int n){
+        vector<vector<vector<string>>> dp(m+1, vector<vector<string>>(n+1, vector<string>()));
+        dp[m][n].push_back(to_string(m) + ':' + to_string(n) + " | ");
+        for (int i = m; i >= 0; i--){
+            for (int j = min(i, n); j >= 0; j--){
+                string str = to_string(i) + ':' + to_string(j) + " | ";
+                if ((i == j && i < m) || (i > j && i < m)){
+                    for (auto k : dp[i + 1][j]){
+                        dp[i][j].push_back(str + k);
+                    }
+                }
+                if (i > j && j < n){
+                    for (auto k : dp[i][j + 1]){
+                        dp[i][j].push_back(str + k);
+                    }
+                }
+            }
+        }
+        return dp[0][0];
+    }
 };
